e1eb103e181fa14e744f323ba439065bf6ee54cc
Muhammad Tausif <tausifasia@gmail.com>
2023-01-14 14:08:03 +0500

    Start program, and click on 100, 100 coordinate.

--- fiverr_automation/priodic_click.py
+++ fiverr_automation/priodic_click.py
@@ -1,7 +1,20 @@
-import pyautogui
+import pyautogui as pui
+import time
+from datetime import datetime as dt
+import random
 
 # Move the mouse to the point
-pyautogui.moveTo(100, 200)
+pui.moveTo(100, 200)
 
 # Click the left mouse button
-pyautogui.click()
+pui.click()
+
+pui.click(button='right')
+
+while(True):
+    pui.moveTo(100, 100)
+    pui.click()
+
+    random_sleep_time = random.uniform(30, 60)
+    time.sleep(random_sleep_time)
+    print(dt.now())
